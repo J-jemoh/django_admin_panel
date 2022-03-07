1d31d57e59408d204f3bbd0a32f4e9168a433613
J-jemoh <44498304+J-jemoh@users.noreply.github.com>
2022-03-07 17:16:49 +0300

    header modificaions

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 from django.http import HttpResponse
 from . import views
 
+
 urlpatterns = [
 path('',views.home, name="home"),
 path('register',views.register, name="register"),
